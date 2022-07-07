e20abdd4ccdf55a45e6dacea24827ed92615a6f5
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-07 07:18:32 +0600

    add new user coding done

--- index.js
+++ index.js
@@ -2,13 +2,40 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.send('Leading Home Route for Ranjit');
 });
+// get from mongodb api
+
+const uri = "mongodb+srv://users:4p0QpTLyFR33djF0@cluster0.kwdbbxh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/* 
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+ */
+async function run(){
+    try{
+        await client.connect();
+        const userCollection = client.db('practice').collection('users');
+        app.post('/user', async(req, res)=>{
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, ()=>{
     console.log(`Listening to port ${port} with Ranjit`);
